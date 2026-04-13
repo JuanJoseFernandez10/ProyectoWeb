cc787c0e48e31632e41245fef9d324a51260ebc6
JuanJoseFernandez10 <fernandez.suarez.juan.jose@iescamas.es>
2026-04-13 21:31:33 +0200

    Update request matchers in SecurityConfig

--- groovelink-backend/groovelink-backend/src/main/java/com/groovelink/configuration/SecurityConfig.java
+++ groovelink-backend/groovelink-backend/src/main/java/com/groovelink/configuration/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/error", "/favicon.ico", "/auth/**").permitAll()
+                        .requestMatchers("/", "/error", "/favicon.ico", "/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
